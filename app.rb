6c92c8e1bf4c40ed0bd882bd1d416e019a15f4d3
Natalie Pearson <natalie@connectedhomes.co.uk>
2013-07-26 15:02:19 +0100

    erb chat ups created

--- app.rb
+++ app.rb
@@ -3,3 +3,20 @@ require 'sinatra'
 get '/' do
     erb :index
 end
+
+get '/:chat/:to/:from' do
+    @from = params[:from]
+    @to = params[:to]
+    if params[:chat]=="alphabet"
+        erb :alphabet
+    elsif params[:chat]=="ticket"
+        erb :ticket
+    elsif params[:chat]=="nerd"
+        erb :nerd
+    end
+end
+
+get '/:to' do
+    @to =params[:to]
+    erb :unique
+end
